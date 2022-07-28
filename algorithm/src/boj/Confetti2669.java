1452616583f4691a08434f72aca8252536babd05
2diotodo <willowbuds62@gmail.com>
2022-07-28 20:38:21 +0900

    soso

---
+++ algorithm/src/boj/Confetti2669.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Confetti2669 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum=0;
+		
+		for(int i=0; i<4; i++) {
+			int square1i = sc.nextInt();
+			
+		}
+	}//end main()
+}//end class
